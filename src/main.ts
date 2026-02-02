540a554c207e3cc172fadec9e7bf240e294ff2e4
Alessandro Marcantoni <alessandro.marcantoni.98@gmail.com>
2026-02-02 23:15:24 +0100

    chore: add version print

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { mount } from "svelte";
 import "./app.css";
 import App from "./App.svelte";
 
+console.log("Frontend build:", import.meta.env.MODE, new Date().toISOString());
+
 const app = mount(App, {
   target: document.getElementById("app")!,
 });
